97bdca449446300a082155a991e077d0ecbbb5c9
Anton Savoskin <asav@dvhb.ru>
2019-06-24 10:11:19 +0700

    fix(figma2svg): replace dest flag with icons flag

--- src/base.ts
+++ src/base.ts
@@ -3,6 +3,6 @@ import { flags, Command } from '@oclif/command';
 export abstract class Base extends Command {
   static flags = {
     help: flags.help({ char: 'h' }),
-    dest: flags.string({ char: 'd', description: 'icons folder', required: true, default: 'icons' }),
+    icons: flags.string({ char: 'i', description: 'icons folder', required: true, default: 'icons' }),
   };
 }
