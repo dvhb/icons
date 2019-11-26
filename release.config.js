76afdd9dd29615fa422e09cecdab8992a2d30b1a
Anton Savoskin <asav@dvhb.ru>
2019-11-26 21:13:11 +0700

    chore: add @semantic-release/git

--- release.config.js
+++ release.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/npm',
-    ['@semantic-release/git', { assets: ['package.json', 'CHANGELOG.md', 'README.md', 'docs'] }],
-  ],
+  plugins: [['@semantic-release/git', { assets: ['package.json', 'CHANGELOG.md', 'README.md', 'docs'] }]],
 };
